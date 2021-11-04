84d5e60e90d67b0c54aff24e2a3f99f70c6020bb
Jack <Riotshielder21@gmail.com>
2021-11-04 14:43:58 +0000

    initial commit

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,10 @@
+# CMake generated Testfile for 
+# Source directory: /home/riotshielder21/catkin_ws/src
+# Build directory: /home/riotshielder21/catkin_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("ros_demo")
+subdirs("ivr_assignment")
+subdirs("ivr_lab")
---
+++ build/ros_demo/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/riotshielder21/catkin_ws/src/ros_demo
+# Build directory: /home/riotshielder21/catkin_ws/build/ros_demo
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
