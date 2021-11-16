8bf7a71d040d21f86a7c0c251afc0b9266939725
Jack <Riotshielder21@gmail.com>
2021-11-16 15:37:35 +0000

    refactor and functions for counturs

--- src/ivr_assignment/src/vision_1.py
+++ src/ivr_assignment/src/vision_1.py
@@ -53,6 +53,7 @@ class image_converter:
         return np.array([x_d, y_d])
         
 #----------------------------------------------------------------------------------------------------------
+    
     # Receive data, process it, and publish yz plane
     def callbackyz(self, data):
         # Receive the image
@@ -68,12 +69,64 @@ class image_converter:
 	    xz_image = self.bridge.imgmsg_to_cv2(data, "bgr8")
 	except CvBridgeError as e:
 	    print(e)
+	     def Contours(self, contours):
+       	Centres = []
+		for c in contours:
+		      # calculate moments for each contour
+		      M = cv2.moments(c)
+		      # calculate y,z coordinate of center
+		      if M["m00"] != 0:
+		            cX = int(M["m10"] / M["m00"])
+		            cY = int(M["m01"] / M["m00"])
+		      else:
+		            cX, cY = 0, 0
+		      
+		      #3m = 93 pixels | 1m = 31 pixels
+		      Centres.append([cY,cZ])
+              return Centres
+          	#link 1 = 4m
+	   	#joint 2 = link 2 = 0m
+	   	#link 2 = 3.2m
+	   	#link 3 = 2.8m
+	        #cv2.circle(image_copy, (cX, cY), 2, (255, 255, 255), -1)
+	        #cv2.putText(image_copy, "centroid", (cX - 25, cY - 25),cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
+   	
+
+#----------------------------------------------------------------------------------------------------------        
+ 
+ #link 1 angle, green to yellow
+        def angles(self, centres):
+        	
+		if (centres[0][0]-centres[1][0]) != 0:
+		      link1 = np.arctan2((centres[0][1]-centres[0][1])/(centres[0][0]-centres[1][0]))
+		      print(link1)
+		else:
+		      link1 = 0
+		
+		#link 2 angle, yellow to blue     
+		if (centres[1][0]-centres[2][0]) != 0:
+		      link3 = np.arctan2((centres[1][1]-centres[2][1])/(centres[1][0]-centres[2][0]))-link1
+		      print(link3)
+		else:
+		      link3 = 0
+		
+		#link 3 angle, blue to red      
+		if (yzCentres[2][0]-centres[3][0]) != 0:
+		      link4 = np.arctan2((centres[2][1]-centres[3][1])/(centres[2][0]-centres[3][0])) -link1 - link3
+		      print(link4)
+		else:
+		      link4 = 0
+		
+		return np.array[link1, link3, link4]
+		#cv2.imshow('Centroids', image_copy)
+		#cv2.waitKey(100)
+
+		#cv2.destroyAllWindows()
+ 
 #----------------------------------------------------------------------------------------------------------
 #----------------------------------------------------------------------------------------------------------       
 
         # Perform image processing, green base joint not required
-        # The image is loaded as cv_imag
-
         
         green_u = (20,256,20)
         green_l = (0,50,0)
@@ -84,87 +137,51 @@ class image_converter:
         yellow_u = (20,256,256)
         yellow_l = (0,50,50)
         
-        yzmaskG = cv2.inRange(yz_image1, green_l, green_u)
-        yzmaskY = cv2.inRange(yz_image1, yellow_l, yellow_u)
-        yzmaskB = cv2.inRange(yz_image1, blue_l, blue_u)
-        yzmaskR = cv2.inRange(yz_image1, red_l, red_u)
+        yzmaskG = cv2.inRange(yz_image, green_l, green_u)
+        yzmaskY = cv2.inRange(yz_image, yellow_l, yellow_u)
+        yzmaskB = cv2.inRange(yz_image, blue_l, blue_u)
+        yzmaskR = cv2.inRange(yz_image, red_l, red_u)
+        xzmaskG = cv2.inRange(xz_image, green_l, green_u)
+        xzmaskY = cv2.inRange(xz_image, yellow_l, yellow_u)
+        xzmaskB = cv2.inRange(xz_image, blue_l, blue_u)
+        xzmaskR = cv2.inRange(xz_image, red_l, red_u)
         
         yzmaskJ1 = cv2.cvtColor(yzmaskG,cv2.COLOR_BGR2RGB)
         yzmaskJ2 = cv2.cvtColor(yzmaskY,cv2.COLOR_BGR2RGB)
         yzmaskJ3 = cv2.cvtColor(yzmaskB,cv2.COLOR_BGR2RGB)
         yzmaskJ4 = cv2.cvtColor(yzmaskR,cv2.COLOR_BGR2RGB)
+        xzmaskJ1 = cv2.cvtColor(xzmaskG,cv2.COLOR_BGR2RGB)
+        xzmaskJ2 = cv2.cvtColor(xzmaskY,cv2.COLOR_BGR2RGB)
+        xzmaskJ3 = cv2.cvtColor(xzmaskB,cv2.COLOR_BGR2RGB)
+        xzmaskJ4 = cv2.cvtColor(xzmaskR,cv2.COLOR_BGR2RGB)
         
         full_frameyz = yz_image & ( yzmaskJ1 | yzmaskJ2 | yzmaskJ3 | yzmaskJ4)
+        full_framexz = xz_image & ( xzmaskJ1 | xzmaskJ2 | xzmaskJ3 | xzmaskJ4)
         
         frame_grayyz = cv2.cvtColor(full_frameyz, cv2.COLOR_RGB2GRAY)
-        ret, joint_thresh = cv2.threshold(frame_grayyz, 1, 255, cv2.THRESH_BINARY)
+        frame_grayxz = cv2.cvtColor(full_frameyz, cv2.COLOR_RGB2GRAY)
+        
+        ret, joint_threshyz = cv2.threshold(frame_grayyz, 1, 255, cv2.THRESH_BINARY)
+        ret, joint_threshxz = cv2.threshold(frame_grayxz, 1, 255, cv2.THRESH_BINARY)
 
-        contours, hierarchy = cv2.findContours(joint_thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+        contoursyz, hierarchyyz = cv2.findContours(joint_threshyz, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+        contoursxz, hierarchyxz = cv2.findContours(joint_threshxz, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
         
         #image_copy = cv_image.copy()
-      
         #cv2.drawContours(image_copy, contours, -1, (255, 255, 255), 2, cv2.LINE_AA)
         #cv2.imshow('Contoured', cv_image)
         #cv2.waitKey(10000)
         
-        jointCentres = []
+        yzCentres = Contours(contoursyz)
+        xzCentres = Contours(contoursxz)
         
- #----------------------------------------------------------------------------------------------------------
-       
-        for c in contours:
-              # calculate moments for each contour
-              M = cv2.moments(c)
-              # calculate x,y coordinate of center
-              if M["m00"] != 0:
-                    cX = int(M["m10"] / M["m00"])
-                    cY = int(M["m01"] / M["m00"])
-              else:
-                    cX, cY = 0, 0
-              
-              #3m = 93 pixels | 1m = 31 pixels
-              jointCentres.append([cX/31,cY/31])
-              #cv2.circle(image_copy, (cX, cY), 2, (255, 255, 255), -1)
-              #cv2.putText(image_copy, "centroid", (cX - 25, cY - 25),cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
-   	
-   	#link 1 = 4m
-   	#joint 2 = link 2 = 0m
-   	#link 2 = 3.2m
-   	#link 3 = 2.8m
-   	
-#----------------------------------------------------------------------------------------------------------        
-        
-        #link 1 angle, green to yellow
-        if (jointCentres[0][0]-jointCentres[1][0]) != 0:
-              link1 = np.arctan2((jointCentres[0][1]-jointCentres[0][1])/(jointCentres[0][0]-jointCentres[1][0]))
-              print(link1)
-        else:
-              link1 = 0
-        
-        #link 2 angle, yellow to blue     
-        if (jointCentres[1][0]-jointCentres[2][0]) != 0:
-              link2 = np.arctan2((jointCentres[1][1]-jointCentres[2][1])/(jointCentres[1][0]-jointCentres[2][0]))-link1
-              print(link2)
-        else:
-              link2 = 0
-        
-        #link 3 angle, blue to red      
-        if (jointCentres[2][0]-jointCentres[3][0]) != 0:
-              link3 = np.arctan2((jointCentres[2][1]-jointCentres[3][1])/(jointCentres[2][0]-jointCentres[3][0])) -link1 - link2
-              print(link3)
-        else:
-              link3 = 0
-        
-        #cv2.imshow('Centroids', image_copy)
-        #cv2.waitKey(100)
-
-        #cv2.destroyAllWindows()
 
 #--------------------------------------------------------------------------------------------------------------
 #--------------------------------------------------------------------------------------------------------------
 
         # change te value of self.joint.data to your estimated value from the images
         self.joints = Float64MultiArray()
-        self.joints.data = np.array([link1,link2, link3])
+        self.joints.data = np.array([link1, link2, link3])
         #  self.link1 = cv2.inRange(cv2.imread('link1.png', 1), (200, 200, 200), (255, 255, 255))
         #  self.link2 = cv2.inRange(cv2.imread('link2.png', 1), (200, 200, 200), (255, 255, 255))
         #  self.link3 = cv2.inRange(cv2.imread('link3.png', 1), (200, 200, 200), (255, 255, 255))
